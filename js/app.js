aa6a5c0e529f8f7f55cc1f5f4652ec9a46a8cae2
George Harrison <futuregamecreator@gmail.com>
2017-10-11 22:30:08 -0400

    smal change

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-//character Selection
-function CharacterSelect() {
-    var startMenu = window.open("", "MsgWindow", "width=200,height=100");
-    myWindow.document.write("<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>");
-}
 // Sets an initial player score of 0.
 var score = 0;
 document.getElementById('playerScore').innerHTML = score;
